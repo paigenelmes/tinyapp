f33e89d7913dcc51c268b48dc51834d6d1bffcbf
Paige Nelmes <designer@paigenelmes.com>
2022-10-29 16:09:24 -0700

    Adding a new URLs template, a POST route for form submission, and the generate random string function

--- express_server.js
+++ express_server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
+//Generate random string function
+const generateRandomString = function() {
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let randomStr = "";
+  for (let i = 6; i > 0; i--) {
+    randomStr += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return randomStr;
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +43,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
