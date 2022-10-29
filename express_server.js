a8bfb1fbccfe2d0820889d84cf6caceb19538026
Paige Nelmes <designer@paigenelmes.com>
2022-10-29 14:07:45 -0700

    Adding a 2nd Route & template

--- express_server.js
+++ express_server.js
@@ -31,6 +31,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params.longURL};
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
