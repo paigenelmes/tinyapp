f6384c7af706932567810442578dd351e926fd0d
Paige Nelmes <designer@paigenelmes.com>
2022-10-30 12:04:37 -0700

    Save new URLs to database and redirect to short URL page. Also redirect any request to /u/:shortURL to its long URL.

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
+//Setting up express & port
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//Setting up middleware
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Setting view engine to EJS
 app.set("view engine", "ejs");
 
 //Generate random string function
@@ -16,43 +20,58 @@ const generateRandomString = function() {
   return randomStr;
 };
 
-
+//URL Database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {id:"b2xVn2", longURL: "http://www.lighthouselabs.ca"},
+  "9sm5xK": {id: "9sm5xK", longURL: "http://www.google.com"}
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+//Displays a message that the server is listening when server is running
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
 
+//Returning JSONs
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//Rendering the template vars into main URL page
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+//Rendering new URLs
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Rendering template vars into /urls/:id page
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id].longURL};
   res.render("urls_show", templateVars);
 });
 
+//Save new URL to URL Database & redirect to short URL page. Generate URL w/ helper function
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = {
+    id: shortURL,
+    longURL: longURL
+  };
+  console.log("Full request body:", req.body); // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);
+});
+
+//Redirect any reqeust to /u/short URL to its long URL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL].longURL;
+  console.log("URL Database", urlDatabase);
+  console.log("Long URL", longURL);
+  res.redirect(longURL);
 });
