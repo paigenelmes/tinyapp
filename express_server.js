5aa0da048e58517b86ded68786c6e70e6ab0d3a3
Paige Nelmes <designer@paigenelmes.com>
2022-11-04 10:58:33 -0700

    Storing passwords securely with bcyrpt

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
 
@@ -36,8 +37,8 @@ const users = {
 
 //URL Database
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "2LOihj"},
-  "9sm5xK": { longURL: "http://www.google.com", userID: "jS3PuW"}
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "2LOihj" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "jS3PuW" }
 };
 
 ///////////////////////////////////
@@ -96,7 +97,7 @@ Display an error if the user is not logged in
 If the user is logged in, display all the URLs that they have created*/
 app.get("/urls", (req, res) => {
   if (!req.cookies["userID"]) {
-    res.status(403).send("Error: Sorry, you must be logged in to see your URLs.");
+    return res.status(403).send("Error: Sorry, you must be logged in to see your URLs.");
   } else {
     const id = req.cookies["userID"];
     const user = users[id];
@@ -134,13 +135,13 @@ app.get("/urls/:id", (req, res) => {
   const user = users[userID];
   const userURLs = urlsForUser(userID, urlDatabase);
   if (!userID) {
-    res.status(403).send("Error: Sorry, this page is not accessible. Please login first.");
+    return res.status(403).send("Error: Sorry, this page is not accessible. Please login first.");
 
   } else if (!urlDatabase[req.params.id]) {
-    res.status(404).send("Error: Sorry, this URL does not exist. Try again.");
+    return res.status(404).send("Error: Sorry, this URL does not exist. Try again.");
 
   } else if (!userURLs[req.params.id]) {
-    res.status(403).send("Error: Sorry, you do not have permission to view this page.");
+    return res.status(403).send("Error: Sorry, you do not have permission to view this page.");
 
   } else {
     const templateVars = {
@@ -156,7 +157,7 @@ app.get("/urls/:id", (req, res) => {
 /If user is not logged in, display an error message*/
 app.post("/urls", (req, res) => {
   if (!req.cookies["userID"]) {
-    res.status(403).send("Error: Sorry, only registered users can create new URLs. Please login or register.");
+    return res.status(403).send("Error: Sorry, only registered users can create new URLs. Please login or register.");
   } else {
     const shortURL = generateRandomString();
     const longURL = req.body.longURL;
@@ -184,7 +185,7 @@ Display an error if the user does not own the URL*/
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userID = req.cookies["userID"];
   if (!userID) {
-    res.status(403).send("Error: Sorry, you are not authorized to delete this URL.");
+    return res.status(403).send("Error: Sorry, you are not authorized to delete this URL.");
   } else {
     const shortURL = req.params.shortURL;
     delete urlDatabase[shortURL];
@@ -197,7 +198,7 @@ Display an error if the user does not own the URL*/
 app.post("/urls/:shortURL/edit", (req, res) => {
   const userID = req.cookies["userID"];
   if (!userID) {
-    res.status(403).send("Error: Sorry, you are not authorized to delete this URL.");
+    return res.status(403).send("Error: Sorry, you are not authorized to delete this URL.");
   } else {
     const shortURL = req.params.shortURL;
     urlDatabase[shortURL].longURL = req.body.newURL;
@@ -233,6 +234,8 @@ Display error if email does exist but the password doesn't match the one in the 
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  const hashPassword = bcrypt.hashSync(password, 10);
+
   if (!email || !password) {
     return res.status(401).send("Error: Please enter an email address and a password.");
   }
@@ -241,7 +244,7 @@ app.post("/login", (req, res) => {
     return res.status(401).send(`Error: A user with the email address ${email} does not exist. Try again.`);
   }
   const existingPass = existingUser.password;
-  if (password !== existingPass) {
+  if (!bcrypt.compareSync(existingPass, hashPassword)) {
     return res.status(401).send("Error: Incorrect password. Try again.");
   }
   res.cookie("userID", existingUser.id);
@@ -259,7 +262,8 @@ app.post("/logout", (req, res) => {
 //////// USER REGISTRATION ///////
 /////////////////////////////////
 
-//Render the templates vars to the user registration page
+/*Render the templates vars to the user registration page
+If logged in, redirect to URLs page*/
 app.get("/register", (req, res) => {
   if (req.cookies["userID"]) {
     res.redirect("/urls");
@@ -275,22 +279,24 @@ app.get("/register", (req, res) => {
 });
 
 /* Save new users to User Database, generate a userID, set cookies & redirect to main urls page
-Display error if email & password are empty, or if email is already in user object */
+Display error if email & password are empty, or if email is already in user object
+Save the hash password with bcyrpt*/
 app.post("/register", (req, res) => {
   const userID = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
+  const hashPassword = bcrypt.hashSync(password, 10);
 
   if (!email || !password) {
-    res.status(401).send("Error: Please enter an email address and a password.");
+    return res.status(401).send("Error: Please enter an email address and a password.");
   } else if (getUserByEmail(email)) {
-    res.status(401).send(`Error: A user with the email address ${email} already exists.`);
+    return res.status(401).send(`Error: A user with the email address ${email} already exists.`);
   }
 
   users[userID] = {
     id: userID,
     email,
-    password
+    password: hashPassword
   };
 
   res.cookie("userID", userID);
